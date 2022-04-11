b2a56d7488c093b41990fe4cd81ff6a5c2d921e3
Luiz Lopes <luizlopes12@outlook.com>
2022-04-11 11:55:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,83 @@
 const express = require('express')
 
+const { randomUUID } = require("crypto")
+
 const app = express()
 
+app.use(express.json())
+
+const products = []
+
+/*
+POST => Inserir
+GET => Buscar
+PUT => Alterar
+DELETE => Remover
+*/
+
+
+/*
+Body => envia dados para a aplicação
+Params => envia os dados pela url /prod/651981951519419
+Query => envia dados para url, mas com formatação chave e valor /prod?id=4851161name=Luiz
+*/
 
 app.get('/', (req,res) =>{
-    return res.json({
-        'message':'Rota inicial'
+    res.send('Servidor Iniciado')
+})
+
+
+app.get('/prod', (req,res) =>{
+    return res.json(products)
+})
+
+
+app.post('/prod', (req,res) =>{
+
+    const { name, price } = req.body;
+
+    const prod = {
+        name,
+        price,
+        id: randomUUID()
+    }
+
+    products.push(prod)
+    return res.json(prod)
+})
+
+app.get('/prod/:id', (req,res) =>{
+    const { id } = req.params
+    res.json(products.find((item)=> item.id === id))
+})
+
+
+app.put('/prod/:id', (req,res)=>{
+    const { id } = req.params
+    const { name, price } = req.body;
+    const itemIndex = products.findIndex((item)=> item.id === id)
+    
+    products[itemIndex] = {
+        ...products[itemIndex],
+        name,
+        price
+    }
+
+    res.json({
+        "message": "Produto alterado"
     })
 })
+
+app.delete('/prod/:id', (req,res)=>{
+    const { id } = req.params
+    const itemIndex = products.findIndex((item)=> item.id === id)
+    products.splice(itemIndex, 1)
+
+    res.json({
+        'message':'Produto removido'
+    })
+})
+
 
 
 
