4d72466b55a34e19bbc77d6b2aca7de9a65b7884
Luiz Lopes <luizlopes12@outlook.com>
2022-05-04 12:40:07 -0300

    configurando db

--- sequelize/src/app.js
+++ sequelize/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const db = require('./config/db')
 const app = express()
 
 app.use(express.json())
--- sequelize/src/config/db.js
+++ sequelize/src/config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 //Conexão com o banco de dados mysql
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize("testes_node", "root", "admin", {
+  host: "localhost",
   dialect: "mysql",
 });
 
