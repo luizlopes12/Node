fe77abfcc8a89feb3236cc9e5697056378d9535f
Luiz Lopes <luizlopes12@outlook.com>
2022-05-04 11:51:50 -0300

    Anotações e variaveis locais

---
+++ sequelize/src/config/db.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+//Conexão com o banco de dados mysql
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Banco de dados conectado"))
+  .catch((err) => console.log("falha ao se conectar: " + err));
+
+module.exports = {
+  Sequelize,
+  sequelize,
+};
